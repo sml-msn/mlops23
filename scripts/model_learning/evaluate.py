de6ca75ac345bd5b81dbec4878724b0daaf10d59
adminvm <shamil1236@gmail.com>
2023-11-06 21:08:45 +0500

    model evaluation

--- scripts/model_learning/evaluate.py
+++ scripts/model_learning/evaluate.py
@@ -22,39 +22,53 @@ def evaluate(model, matrix, split, live, save_path):
         live (dvclive.Live): Dvclive instance.
         save_path (str): Path to save the metrics.
     """
-    labels = matrix[:, 1].toarray().astype(int)
-    x = matrix[:, 2:]
-
-    predictions_by_class = model.predict_proba(x)
-    predictions = predictions_by_class[:, 1]
+    labels = matrix.iloc[:,0]
+    X = matrix.iloc[:,[1,2,3]].to_numpy(dtype='float32')
+    predictions = model.predict(X, check_input=False)
 
     # Use dvclive to log a few simple metrics...
-    avg_prec = metrics.average_precision_score(labels, predictions)
-    roc_auc = metrics.roc_auc_score(labels, predictions)
+    accuracy = metrics.accuracy_score(labels, predictions)
+    precision = metrics.precision_score(labels, predictions)
+    recall = metrics.recall_score(labels, predictions)
+    f1_score = metrics.f1_score(labels, predictions)
+    
     if not live.summary:
-        live.summary = {"avg_prec": {}, "roc_auc": {}}
-    live.summary["avg_prec"][split] = avg_prec
-    live.summary["roc_auc"][split] = roc_auc
-
+        live.summary = {"accuracy": {}, "precision": {}, "recall": {}, "f1_score": {}}
+    
+    live.summary["accuracy"][split] = accuracy
+    live.summary["precision"][split] = precision
+    live.summary["recall"][split] = recall
+    live.summary["f1_score"][split] = f1_score
+    
+    # live.log_sklearn_plot("accuracy", labels, predictions, name=f"roc/{split}")
+    # live.log_sklearn_plot("precision", labels, predictions, name=f"roc/{split}")
+    # live.log_sklearn_plot("recall", labels, predictions, name=f"roc/{split}")
+    # live.log_sklearn_plot("f1_score", labels, predictions, name=f"roc/{split}")
+    
     # ... and plots...
     # ... like an roc plot...
-    live.log_sklearn_plot("roc", labels, predictions, name=f"roc/{split}")
+    
+    #live.log_sklearn_plot("roc", labels, predictions, name=f"roc/{split}")
+    
     # ... and precision recall plot...
     # ... which passes `drop_intermediate=True` to the sklearn method...
-    live.log_sklearn_plot(
-        "precision_recall",
-        labels,
-        predictions,
-        name=f"prc/{split}",
-        drop_intermediate=True,
-    )
+    
+    # live.log_sklearn_plot(
+        # "precision_recall",
+        # labels,
+        # predictions,
+        # name=f"prc/{split}",
+        # drop_intermediate=True,
+    # )
+    
     # ... and confusion matrix plot
-    live.log_sklearn_plot(
-        "confusion_matrix",
-        labels.squeeze(),
-        predictions_by_class.argmax(-1),
-        name=f"cm/{split}",
-    )
+    
+    # live.log_sklearn_plot(
+        # "confusion_matrix",
+        # labels.squeeze(),
+        # predictions_by_class.argmax(-1),
+        # name=f"cm/{split}",
+    # )
 
 
 def save_importance_plot(live, model, feature_names):
@@ -85,27 +99,30 @@ def main():
         sys.stderr.write("\tpython evaluate.py model features\n")
         sys.exit(1)
 
-    model_file = sys.argv[1]
-    train_file = os.path.join(sys.argv[2], "train.pkl")
-    test_file = os.path.join(sys.argv[2], "test.pkl")
+    model_file = sys.argv[2]
+    #train_file = os.path.join(sys.argv[2], "train.pkl")
+    #test_file = os.path.join(sys.argv[2], "test.pkl")
+    test_file = os.path.join(sys.argv[1])
 
     # Load model and data.
     with open(model_file, "rb") as fd:
         model = pickle.load(fd)
 
-    with open(train_file, "rb") as fd:
-        train, feature_names = pickle.load(fd)
+    #with open(train_file, "rb") as fd:
+    #    train, feature_names = pickle.load(fd)
 
-    with open(test_file, "rb") as fd:
-        test, _ = pickle.load(fd)
+    #with open(test_file, "rb") as fd:
+        #test, _ = pickle.load(fd)
+    
+    test = pd.read_csv(test_file, header=None)
 
     # Evaluate train and test datasets.
     with Live(EVAL_PATH, dvcyaml=False) as live:
-        evaluate(model, train, "train", live, save_path=EVAL_PATH)
+        #evaluate(model, train, "train", live, save_path=EVAL_PATH)
         evaluate(model, test, "test", live, save_path=EVAL_PATH)
 
         # Dump feature importance plot.
-        save_importance_plot(live, model, feature_names)
+        #save_importance_plot(live, model, feature_names)
 
 
 if __name__ == "__main__":
